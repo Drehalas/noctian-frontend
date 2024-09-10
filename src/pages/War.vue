c3e92bf6977cf1b71d05d12ed5c0817aca003a35
ersinhamzakose <ehkproje@gmail.com>
2024-09-10 12:04:23 +0300

    war sayfası dinamik hale getirildi

--- src/pages/War.vue
+++ src/pages/War.vue
@@ -23,13 +23,13 @@
                         <path d="M0 0.929443H180" stroke="white" stroke-width="0.3" />
                     </svg>
                 </div>
-                <div class="group-16-gfXRIF">
+                <div class="group-16-gfXRIF" v-for="(throne, index) in throneList" :key="throne.id">
                     <div class="rectangle-10-sMV5yX rectangle-10"></div>
                     <div class="frame-16-sMV5yX frame-16">
-                        <div class="frame-17-vwdZa2 frame-17">
-                            <div class="orcs roboto-semi-bold-white-12px">ORCS</div>
+                        <div class="frame-17-vwdZa2 frame-17" :style="{ backgroundColor: getWarBackgroundColor(throne.faction) }">
+                            <div class="orcs roboto-semi-bold-white-12px">{{ throne.faction }}</div>
                             <img class="x9b3af883-354d-4413-bf7f-37baaad350d0-2-91MWnz x9b3af883-354d-4413-bf7f-37baaad350d0-2"
-                                src="@/assets/Global/War/Orc logo.png" alt="Orc logo">
+                                :src="throne.imageUrl">
                         </div>
                         <div class="frame-18-vwdZa2 frame-18">
                             <div class="frame-4">
@@ -37,167 +37,30 @@
                                         class="pngtreeglossy-golden-coin-icon_6866281-1"
                                         src="@/assets/Global/Common/Gold.png"
                                         alt="Pngtreeglossy golden coin icon_6866281 1"></div>
-                                <div class="x32102-b-qFxTKI roboto-semi-bold-white-11px">321.02B
-                                </div>
+                                <div class="x32102-b-qFxTKI roboto-semi-bold-white-11px">{{ throne.totalGold }}</div>
                             </div>
                         </div>
-                        <div class="frame-19-vwdZa2 frame-19">
-                            <div class="x1-RBnjXB roboto-semi-bold-white-12px">1.</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="group-28-gfXRIF">
-                    <div class="rectangle-10-2prqx0 rectangle-10"></div>
-                    <div class="frame-16-2prqx0 frame-16">
-                        <div class="frame-17-xwffxI frame-17">
-                            <div class="demons roboto-semi-bold-white-12px">DEMONS</div>
-                            <img class="x9b3af883-354d-4413-bf7f-37baaad350d0-2-sAYRXY x9b3af883-354d-4413-bf7f-37baaad350d0-2"
-                                src="@/assets/Global/War/Demon logo.png" alt="Demon logo">
-                        </div>
-                        <div class="frame-18-xwffxI frame-18">
-                            <div class="frame-4">
-                                <div class="group-8-XJQBxg group-8"><img
-                                        class="pngtreeglossy-golden-coin-icon_6866281-1"
-                                        src="@/assets/Global/Common/Gold.png"
-                                        alt="Pngtreeglossy golden coin icon_6866281 1"></div>
-                                <div class="x31898-b-XJQBxg roboto-semi-bold-white-11px">318.98B
-                                </div>
-                            </div>
-                        </div>
-                        <div class="frame-19-xwffxI frame-19">
-                            <div class="x2-pPna1m roboto-semi-bold-white-12px">2.</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="group-29-gfXRIF">
-                    <div class="rectangle-10-pxZmSF rectangle-10"></div>
-                    <div class="frame-16-pxZmSF frame-16">
-                        <div class="frame-17-nOdZKA frame-17">
-                            <div class="angels roboto-semi-bold-white-12px">ANGELS</div>
-                            <img class="x59193c11-9086-446e-a544-d8ed9f2e49e8-1-vXwrHo x59193c11-9086-446e-a544-d8ed9f2e49e8-1"
-                                src="@/assets/Global/War/Angel logo.png" alt="Angel logo">
-                        </div>
-                        <div class="frame-18-nOdZKA frame-18">
-                            <div class="frame-4">
-                                <div class="group-8-5eYUB9 group-8">
-                                    <img class="pngtreeglossy-golden-coin-icon_6866281-1"
-                                        src="@/assets/Global/Common/Gold.png"
-                                        alt="Pngtreeglossy golden coin icon_6866281 1">
-                                </div>
-                                <div class="x31651-b-5eYUB9 roboto-semi-bold-white-11px">316.51B
-                                </div>
-                            </div>
-                        </div>
-                        <div class="frame-19-nOdZKA frame-19">
-                            <div class="x3-lkT0WM roboto-semi-bold-white-12px">3.</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="group-30-gfXRIF">
-                    <div class="rectangle-10-k15xbP rectangle-10"></div>
-                    <div class="frame-16-k15xbP frame-16">
-                        <div class="frame-17-5cxvYG frame-17">
-                            <div class="elves roboto-semi-bold-white-12px">ELVES</div>
-                            <img class="x59193c11-9086-446e-a544-d8ed9f2e49e8-1-v93PyF x59193c11-9086-446e-a544-d8ed9f2e49e8-1"
-                                src="@/assets/Global/War/Elf logo.png" alt="Elf logo">
-                        </div>
-                        <div class="frame-18-5cxvYG frame-18">
-                            <div class="frame-4">
-                                <div class="group-8-dE3Dxo group-8"><img
-                                        class="pngtreeglossy-golden-coin-icon_6866281-1"
-                                        src="@/assets/Global/Common/Gold.png"
-                                        alt="Pngtreeglossy golden coin icon_6866281 1"></div>
-                                <div class="x31427-b-dE3Dxo roboto-semi-bold-white-11px">314.27B
-                                </div>
-                            </div>
-                        </div>
-                        <div class="frame-19-5cxvYG frame-19">
-                            <div class="x4-4r96bM roboto-semi-bold-white-12px">4.</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="group-32-gfXRIF">
-                    <div class="rectangle-10-cgtvab rectangle-10"></div>
-                    <div class="frame-16-cgtvab frame-16">
-                        <div class="frame-17-rKFCBu frame-17">
-                            <div class="humans roboto-semi-bold-white-12px">HUMANS</div>
-                            <img class="x59193c11-9086-446e-a544-d8ed9f2e49e8-1-3ZzqFd x59193c11-9086-446e-a544-d8ed9f2e49e8-1"
-                                src="@/assets/Global/War/Human logo.png" alt="Human logo">
-                        </div>
-                        <div class="frame-18-rKFCBu frame-18">
-                            <div class="frame-4">
-                                <div class="group-8-ruULly group-8"><img
-                                        class="pngtreeglossy-golden-coin-icon_6866281-1"
-                                        src="@/assets/Global/Common/Gold.png"
-                                        alt="Pngtreeglossy golden coin icon_6866281 1"></div>
-                                <div class="x31151-b-ruULly roboto-semi-bold-white-11px">311.51B
-                                </div>
-                            </div>
-                        </div>
-                        <div class="frame-19-rKFCBu frame-19">
-                            <div class="x5-sDaOSL roboto-semi-bold-white-12px">5.</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="group-47-gfXRIF">
-                    <div class="rectangle-10-P5r8SD rectangle-10"></div>
-                    <div class="frame-16-P5r8SD frame-16">
-                        <div class="frame-17-EpcF0b frame-17">
-                            <div class="undeads roboto-semi-bold-white-12px">UNDEADS</div>
-                            <img class="x59193c11-9086-446e-a544-d8ed9f2e49e8-1-a75J6v x59193c11-9086-446e-a544-d8ed9f2e49e8-1"
-                                src="@/assets/Global/War/Undead logo.png" alt="Undead logo">
-                        </div>
-                        <div class="frame-18-EpcF0b frame-18">
-                            <div class="frame-4">
-                                <div class="group-8-Ny3Txx group-8"><img
-                                        class="pngtreeglossy-golden-coin-icon_6866281-1"
-                                        src="@/assets/Global/Common/Gold.png"
-                                        alt="Pngtreeglossy golden coin icon_6866281 1"></div>
-                                <div class="x27474-b-Ny3Txx roboto-semi-bold-white-11px">274.74B
-                                </div>
-                            </div>
-                        </div>
-                        <div class="frame-19-EpcF0b frame-19">
-                            <div class="x6-N2lhx0 roboto-semi-bold-white-12px">6.</div>
+                        <div class="frame-19-vwdZa2 frame-19" :style="{ backgroundColor: getThronePlaceColor(index + 1) }">
+                            <div class="x1-RBnjXB roboto-semi-bold-white-12px">{{ index + 1 }}.</div>
                         </div>
                     </div>
                 </div>
                 <img class="x2-2-gfXRIF" src="@/assets/Global/War/2 2.png" alt="2 2">
-                <div class="group-39-gfXRIF">
-                    <div class="rectangle-10-9mhVzY rectangle-10"></div>
-                    <div class="frame-16-9mhVzY frame-16">
-                        <div class="frame-19-C1Ze1Q frame-19">
-                            <div class="orcs roboto-semi-bold-white-12px">ORCS</div>
+                <div style="position: absolute; top: 80px; width: 100%;left: 0;">
+                    <div class="group-39-gfXRIF" style="position: relative;" v-for="war in warList" :key="war.id">
+                        <div class="rectangle-10-9mhVzY rectangle-10"></div>
+                        <div class="frame-16-9mhVzY frame-16">
+                            <div class="frame-19-C1Ze1Q frame-19"
+                                :style="{ backgroundColor: getWarBackgroundColor(war.first) }">
+                                <div class="orcs roboto-semi-bold-white-12px">{{ war.first }}</div>
+                            </div>
+                            <div class="frame-20-C1Ze1Q frame-20"
+                                :style="{ backgroundColor: getWarBackgroundColor(war.second) }">
+                                <div class="elves roboto-semi-bold-white-12px">{{ war.second }}</div>
+                            </div>
                         </div>
-                        <div class="frame-20-C1Ze1Q frame-20">
-                            <div class="elves roboto-semi-bold-white-12px">ELVES</div>
-                        </div>
+                        <img class="x22" src="@/assets/Global/War/22.png" alt="22">
                     </div>
-                    <img class="x22" src="@/assets/Global/War/22.png" alt="22">
-                </div>
-                <div class="group-40-gfXRIF">
-                    <div class="rectangle-10-LprqiJ rectangle-10"></div>
-                    <div class="frame-16-LprqiJ frame-16">
-                        <div class="frame-19-ekdxd7 frame-19">
-                            <div class="demons roboto-semi-bold-white-12px">DEMONS</div>
-                        </div>
-                        <div class="frame-20-ekdxd7 frame-20">
-                            <div class="angels roboto-semi-bold-white-12px">ANGELS</div>
-                        </div>
-                    </div>
-                    <img class="x22" src="@/assets/Global/War/22.png" alt="22">
-                </div>
-                <div class="group-41-gfXRIF">
-                    <div class="rectangle-10-1epfzU rectangle-10"></div>
-                    <div class="frame-16-1epfzU frame-16">
-                        <div class="frame-20-UxBtug frame-20">
-                            <div class="undeads roboto-semi-bold-white-12px">UNDEADS</div>
-                        </div>
-                        <div class="frame-21-UxBtug">
-                            <div class="humans roboto-semi-bold-white-12px">HUMANS</div>
-                        </div>
-                    </div>
-                    <img class="x22" src="@/assets/Global/War/22.png" alt="22">
                 </div>
                 <div class="group-45-gfXRIF">
                     <div class="rectangle-10-OxckLq rectangle-10"></div>
@@ -227,7 +90,50 @@ export default {
     },
     data() {
         return {
-            endTime: new Date('2024-09-09T00:00:00').getTime(),
+            warList: [
+                {
+                    id: 1,
+                    first: "ORCS",
+                    second: "ELVES"
+                },
+                {
+                    id: 2,
+                    first: "DEMONS",
+                    second: "ANGELS"
+                },
+                {
+                    id: 3,
+                    first: "UNDEADS",
+                    second: "HUMANS"
+                }
+            ],
+            throneList: [
+                {
+                    faction: "ORCS",
+                    totalGold: "321.02B"
+                },
+                {
+                    faction: "DEMONS",
+                    totalGold: "318.98B"
+                },
+                {
+                    faction: "ANGELS",
+                    totalGold: "316.51B"
+                },
+                {
+                    faction: "ELVES",
+                    totalGold: "314.27B"
+                },
+                {
+                    faction: "HUMANS",
+                    totalGold: "311.51B"
+                },
+                {
+                    faction: "UNDEADS",
+                    totalGold: "274.74B"
+                }
+            ],
+            endTime: new Date('2024-09-12T00:00:00').getTime(),
             hours: 0,
             minutes: 0,
             seconds: 0
@@ -251,11 +157,57 @@ export default {
                 this.minutes = 0
                 this.seconds = 0
             }
-        }
+        },
+        getWarBackgroundColor(faction) {
+            switch (faction) {
+                case "ORCS":
+                    return "#073d029e";
+                case "ELVES":
+                    return "#0c9a0080";
+                case "DEMONS":
+                    return "#8a000080";
+                case "ANGELS":
+                    return "#c9d2e280";
+                case "UNDEADS":
+                    return "#21242980";
+                case "HUMANS":
+                    return "#05337e80";
+
+                default:
+                    return "#073d029e";
+            }
+        },
+        getThronePlaceColor(place) {
+            switch (place) {
+                case 1:
+                    return "#ffe10080";
+                case 2:
+                    return "#f0f0f080";
+                case 3:
+                    return "#9b520080";
+                default:
+                    return "#21242980";
+            }
+        },
+        async loadImages() {
+            for (let throne of this.throneList) {
+                throne.imageUrl = (await import(`@/assets/Global/War/${this.formatFactionType(throne.faction)} logo.png`)).default;
+            }
+        },
+        formatFactionType(type) {
+            if(type == "ELVES") {
+                type = "ELFS";
+            }
+
+            type = type.substring(0, type.length - 1)
+
+            return type.toLowerCase().replace(/^\w/, (c) => c.toUpperCase());
+        },
     },
     mounted() {
-        this.updateCountdown()
-        this.timer = setInterval(this.updateCountdown, 1000)
+        this.updateCountdown();
+        this.timer = setInterval(this.updateCountdown, 1000);
+        this.loadImages();
     }
 };
 </script>
