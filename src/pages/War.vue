70fd0b07c858c071ed2dbb06576fc2a8979c4e86
ersinhamzakose <ehkproje@gmail.com>
2024-09-11 16:39:13 +0300

    war, treasure ekranları responsive hale getirildi

--- src/pages/War.vue
+++ src/pages/War.vue
@@ -3,50 +3,48 @@
         <div class="orc-war screen ">
             <div class="frame-30-0Yffcq">
                 <div class="rectangle-3-gfXRIF"></div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="428" height="597" viewBox="0 0 428 597" fill="none"
-                    style="top: 420px;position: absolute;z-index: 0;">
-                    <path d="M0 50C0 22.3858 22.3858 0 50 0H378C405.614 0 428 22.3858 428 50V598H0V50Z"
-                        fill="url(#paint0_linear_37_17472)" />
-                    <defs>
-                        <linearGradient id="paint0_linear_37_17472" x1="214" y1="0" x2="214" y2="598"
-                            gradientUnits="userSpaceOnUse">
-                            <stop stop-color="#29323F" offset="0%" />
-                            <stop offset="100%" stop-color="#C7C7C7" />
-                        </linearGradient>
-                    </defs>
-                </svg>
-                <img class="war-banner-gfXRIF" src="@/assets/Global/War/war banner.png" alt="war banner">
-                <div class="group-27-gfXRIF">
-                    <div class="level-120-JSjuIa" style="font-size: 24px;">THRONE</div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="180" height="2" viewBox="0 0 180 2" fill="none"
-                        style="position: relative;top:15px">
-                        <path d="M0 0.929443H180" stroke="white" stroke-width="0.3" />
-                    </svg>
+                <div
+                    style="top: 45vh;position: absolute;z-index: 0;width: 100vw;min-height: 70vh;background: linear-gradient(180deg, #29323F 0%, #c7c7c7 100%);">
                 </div>
-                <div class="group-16-gfXRIF" v-for="(throne, index) in throneList" :key="throne.id">
-                    <div class="rectangle-10-sMV5yX rectangle-10"></div>
-                    <div class="frame-16-sMV5yX frame-16">
-                        <div class="frame-17-vwdZa2 frame-17" :style="{ backgroundColor: getWarBackgroundColor(throne.faction) }">
-                            <div class="orcs roboto-semi-bold-white-12px">{{ throne.faction }}</div>
-                            <img class="x9b3af883-354d-4413-bf7f-37baaad350d0-2-91MWnz x9b3af883-354d-4413-bf7f-37baaad350d0-2"
-                                :src="throne.imageUrl">
-                        </div>
-                        <div class="frame-18-vwdZa2 frame-18">
-                            <div class="frame-4">
-                                <div class="group-8-qFxTKI group-8"><img
-                                        class="pngtreeglossy-golden-coin-icon_6866281-1"
-                                        src="@/assets/Global/Common/Gold.png"
-                                        alt="Pngtreeglossy golden coin icon_6866281 1"></div>
-                                <div class="x32102-b-qFxTKI roboto-semi-bold-white-11px">{{ throne.totalGold }}</div>
+                <img class="war-banner-gfXRIF" src="@/assets/Global/War/war banner.png" alt="war banner">
+                <div
+                    style="top: 50vh;position: absolute;width: 100vw;min-height: 70vh;display: flex;align-items: center;flex-direction: column;">
+                    <div class="group-27-gfXRIF">
+                        <div class="level-120-JSjuIa" style="font-size: 24px;">THRONE</div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="180" height="2" viewBox="0 0 180 2" fill="none"
+                            style="position: relative;top:15px">
+                            <path d="M0 0.929443H180" stroke="white" stroke-width="0.3" />
+                        </svg>
+                    </div>
+                    <div class="group-16-gfXRIF" v-for="(throne, index) in throneList" :key="throne.id">
+                        <div class="rectangle-10-sMV5yX rectangle-10"></div>
+                        <div class="frame-16-sMV5yX frame-16">
+                            <div class="frame-17-vwdZa2 frame-17"
+                                :style="{ backgroundColor: getWarBackgroundColor(throne.faction) }">
+                                <div class="orcs roboto-semi-bold-white-12px">{{ throne.faction }}</div>
+                                <img class="x9b3af883-354d-4413-bf7f-37baaad350d0-2-91MWnz x9b3af883-354d-4413-bf7f-37baaad350d0-2"
+                                    :src="throne.imageUrl">
                             </div>
-                        </div>
-                        <div class="frame-19-vwdZa2 frame-19" :style="{ backgroundColor: getThronePlaceColor(index + 1) }">
-                            <div class="x1-RBnjXB roboto-semi-bold-white-12px">{{ index + 1 }}.</div>
+                            <div class="frame-18-vwdZa2 frame-18">
+                                <div class="frame-4">
+                                    <div class="group-8-qFxTKI group-8"><img
+                                            class="pngtreeglossy-golden-coin-icon_6866281-1"
+                                            src="@/assets/Global/Common/Gold.png"
+                                            alt="Pngtreeglossy golden coin icon_6866281 1"></div>
+                                    <div class="x32102-b-qFxTKI roboto-semi-bold-white-11px">{{ throne.totalGold }}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="frame-19-vwdZa2 frame-19"
+                                :style="{ backgroundColor: getThronePlaceColor(index + 1) }">
+                                <div class="x1-RBnjXB roboto-semi-bold-white-12px">{{ index + 1 }}.</div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <img class="x2-2-gfXRIF" src="@/assets/Global/War/2 2.png" alt="2 2">
-                <div style="position: absolute; top: 80px; width: 100%;left: 0;">
+                <div
+                    style="position: absolute;top: 30px;width: 100%;left: 0px;display: flex;flex-direction: column;align-items: center;">
+                    <img class="x2-2-gfXRIF" src="@/assets/Global/War/2 2.png" alt="2 2">
                     <div class="group-39-gfXRIF" style="position: relative;" v-for="war in warList" :key="war.id">
                         <div class="rectangle-10-9mhVzY rectangle-10"></div>
                         <div class="frame-16-9mhVzY frame-16">
@@ -54,21 +52,24 @@
                                 :style="{ backgroundColor: getWarBackgroundColor(war.first) }">
                                 <div class="orcs roboto-semi-bold-white-12px">{{ war.first }}</div>
                             </div>
+                            <img class="x22" src="@/assets/Global/War/22.png" alt="22">
+
                             <div class="frame-20-C1Ze1Q frame-20"
                                 :style="{ backgroundColor: getWarBackgroundColor(war.second) }">
                                 <div class="elves roboto-semi-bold-white-12px">{{ war.second }}</div>
                             </div>
                         </div>
-                        <img class="x22" src="@/assets/Global/War/22.png" alt="22">
                     </div>
-                </div>
-                <div class="group-45-gfXRIF">
-                    <div class="rectangle-10-OxckLq rectangle-10"></div>
-                    <div class="frame-16-OxckLq frame-16">
-                        <div class="frame-19-tkD6WF frame-19">
-                            <h1 class="title-JWTY9q" style="font-size: 25px;">NEXT BATTLE:</h1>
-                            <div class="x15958-JWTY9q" style="font-size: 30px;">
-                                {{ formatTime(hours) }}:{{ formatTime(minutes) }}:{{ formatTime(seconds) }}
+
+                    <div class="group-45-gfXRIF">
+                        <div class="rectangle-10-OxckLq rectangle-10">
+                            <div class="frame-16-OxckLq frame-16">
+                                <div class="frame-19-tkD6WF frame-19">
+                                    <h1 class="title-JWTY9q" style="font-size: 25px;">NEXT BATTLE:</h1>
+                                    <div class="x15958-JWTY9q" style="font-size: 30px;">
+                                        {{ formatTime(hours) }}:{{ formatTime(minutes) }}:{{ formatTime(seconds) }}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -195,7 +196,7 @@ export default {
             }
         },
         formatFactionType(type) {
-            if(type == "ELVES") {
+            if (type == "ELVES") {
                 type = "ELFS";
             }
 
